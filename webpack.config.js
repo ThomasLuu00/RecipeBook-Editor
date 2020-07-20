72a6674578fc66e0dac2e1befa340dc974bee755
ThomasLuu00 <Thomas.Luu00@gmail.com>
2020-07-20 11:44:07 -0400

    Changed build path to static

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     entry: './app.js',
 
     output: {
-        path: path.resolve( __dirname, 'dist' ),
+        path: path.resolve( __dirname, 'static' ),
         filename: 'bundle.js'
     },
 
